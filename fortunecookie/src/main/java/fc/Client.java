0f96163d96399c94b15b40ed3b39005d2eb6900d
Chan Yew Yen <chan.yewyen@gmail.com>
2021-12-12 11:19:49 +0800

    Use thread to listen for server response
    Previous implementation waited for 1 s for response from server
    in a while loop with retries
    Changed to Reader and Writer for IO
    Use closeAll function to gracefully shutdown resources
    instead of nested try

--- fortunecookie/src/main/java/fc/Client.java
+++ fortunecookie/src/main/java/fc/Client.java
@@ -1,85 +1,117 @@
 package fc;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ConnectException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private String host;
     private int port;
     private final String COOKIE_PREFIX = "cookie-text ";
-    private final int RETRY_LIMIT = 100;
+    private final String PROMPT = "> ";
     private final long CONNECT_RETRY_WAIT_MS = 1000;
-    private final long RESPONSE_RETRY_WAIT_MS = 10;
     private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
 
     public Client(String host, int port) {
         this.host = host;
         this.port = port;
-    }
-
-    public void start() throws IOException, InterruptedException {
         System.out.println("Starting client...");
         System.out.printf("Connecting to %s at port %d%n", this.host, this.port);
         do {
             try {
                 this.socket = new Socket(this.host, this.port);
-            } catch (ConnectException e) {
+                this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+                this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+            } catch (ConnectException connectionException) {
                 System.err.printf("Failed to connect to %s:%d. Retrying in %d ms%n", this.host, this.port, CONNECT_RETRY_WAIT_MS);
                 try {
                     Thread.sleep(CONNECT_RETRY_WAIT_MS);
                 } catch (InterruptedException interruptedException) {
-
+                    interruptedException.printStackTrace();
                 }
+            } catch (IOException ioException) {
+                closeAll(this.socket, this.bufferedReader, this.bufferedWriter, ioException);
             }
         } while (this.socket == null);
-
         System.out.printf("Connected to %s:%d from local port %d%n", this.socket.getInetAddress(), this.socket.getPort(), this.socket.getLocalPort());
-        try (DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(this.socket.getOutputStream()))) {
+    }
 
-            try (DataInputStream serverResponse = new DataInputStream(new BufferedInputStream(this.socket.getInputStream()))) {
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-                String msg = "";
-                String serverMsg = "";
-                while (!"close".equals(msg.trim())) {
-                    int retries = 0;
-                    System.out.print("> ");
-                    msg = reader.readLine();
-                    dos.writeUTF(msg);
-                    dos.flush();
-                    // wait for server response
-                    while (serverResponse.available() == 0 && retries < RETRY_LIMIT) {
-                        retries++;
-                        Thread.sleep(RESPONSE_RETRY_WAIT_MS);
-                    }
-                    while (serverResponse.available() > 0) {
-                        serverMsg = serverResponse.readUTF();
-                        // System.out.println("Server response: " + serverMsg);
-                        if (serverMsg.startsWith(COOKIE_PREFIX)) {
-                            String cookieText = serverMsg.substring(COOKIE_PREFIX.length());
-                            System.out.println("╘═{*} " + cookieText);
-                        }
-                    }
+    public void sendCommands(){
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        try {
+            while (!"close".equals(command.trim()) && this.socket.isConnected()) {
+                if (!"get-cookie".equals(command.trim())) {
+                    // if user typed get-cookie command, wait to display the cookie text
+                    System.out.print(PROMPT);
                 }
-
-                System.out.println("Closing client connection");
-                this.socket.close();
+                command = scanner.nextLine();
+                this.bufferedWriter.write(command);
+                this.bufferedWriter.newLine();
+                this.bufferedWriter.flush();
             }
+        } catch (IOException e) {
+            closeAll(this.socket, this.bufferedReader, this.bufferedWriter, e);
         }
+        scanner.close();
+    }
 
+    public void listenForResponse() {
+        new Thread(() -> {
+            String serverResponse;
+            while (this.socket.isConnected()) {
+                try {
+                    serverResponse = this.bufferedReader.readLine();
+                    if (serverResponse.startsWith(COOKIE_PREFIX)) {
+                        String cookieText = serverResponse.substring(COOKIE_PREFIX.length());
+                        System.out.println("╘═{*} " + cookieText);
+                        // show the prompt for the next command
+                        System.out.print(PROMPT);
+                    }
+                } catch (IOException e) {
+                    closeAll(this.socket, this.bufferedReader, this.bufferedWriter, e);
+                }
+            }
+        }).start();
+    }
 
+    private void closeAll(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter, Exception exception) {
+        exception.printStackTrace();
+        System.out.println("Closing client connection");
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+
+            if (socket != null) {
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        Client client = new Client("localhost", 3000);
-        client.start();
+        if (args.length > 0) {
+            String[] tokens = args[0].split(":");
+            String host = tokens[0];
+            int port = Integer.parseInt(tokens[1]);
+
+            Client client = new Client(host, port);
+            client.listenForResponse();
+            client.sendCommands();
+        }
     }
 }
