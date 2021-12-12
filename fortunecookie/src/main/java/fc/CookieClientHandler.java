ac6945d7f2918576a0c2a903bd7237caa72c40bf
Chan Yew Yen <chan.yewyen@gmail.com>
2021-12-12 11:22:55 +0800

    Refactor to CookieClientHandler class
    Threading to support concurrent client connections
    Use Reader and Writer for IO

---
+++ fortunecookie/src/main/java/fc/CookieClientHandler.java
@@ -0,0 +1,83 @@
+package fc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class CookieClientHandler implements Runnable {
+    private String dataFile;
+    private Socket socket;
+    private Cookie cookie;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public CookieClientHandler(Socket socket, String dataFile) {
+        try {
+            this.socket = socket;
+            this.dataFile = dataFile;
+            this.cookie = new Cookie(this.dataFile);
+            cookie.load();
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            closeAll(this.socket, this.bufferedReader, this.bufferedWriter, e);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (this.socket.isConnected()) {
+            try {
+                messageFromClient = this.bufferedReader.readLine();
+                System.out.printf("Received from [%s:%d]: %s%n", this.socket.getInetAddress(), this.socket.getPort(), messageFromClient);
+
+                if (messageFromClient != null && "get-cookie".equals(messageFromClient.trim())) {
+                    sendCookie();
+                }
+            } catch (IOException e) {
+                closeAll(this.socket, this.bufferedReader, this.bufferedWriter, e);
+                // break;
+            }
+        }
+    }
+
+    private void sendCookie() {
+        System.out.printf("Sending cookie to client [%s:%d]...%n", this.socket.getInetAddress(), this.socket.getPort());
+        String msgToClient = "cookie-text " + this.cookie.get();
+        System.out.println("└─: " + msgToClient);
+        try {
+            this.bufferedWriter.write(msgToClient);
+            this.bufferedWriter.newLine();
+            this.bufferedWriter.flush();
+        } catch (IOException e) {
+            closeAll(this.socket, this.bufferedReader, this.bufferedWriter, e);
+        }
+    }
+
+    private void closeAll(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter, Exception exception) {
+        exception.printStackTrace();
+        System.out.println("Closing server client handler");
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+
+            if (socket != null) {
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- fortunecookie/src/main/java/fc/Server.java
+++ fortunecookie/src/main/java/fc/Server.java
@@ -1,9 +1,5 @@
 package fc;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,43 +7,43 @@ import java.net.Socket;
 public class Server {
     private int port;
     private String dataFile;
+    private ServerSocket serverSocket;
+
 
     public Server(int port, String dataFile) {
         this.port = port;
         this.dataFile = dataFile;
+        try {
+            this.serverSocket = new ServerSocket(this.port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void start() throws IOException {
+    public void start() {
         System.out.println("Starting server...");
-        Cookie cookie = new Cookie(dataFile);
-        cookie.load();
-        try (ServerSocket serverSocket = new ServerSocket(this.port)) {
-            Socket clientSocket = serverSocket.accept();
-            // PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-            try (DataOutputStream out = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()))) {
+        try {
+            while (!this.serverSocket.isClosed()) {
+                Socket socket = this.serverSocket.accept();
+                System.out.println("Listening on port: " + socket.getLocalPort());
+                CookieClientHandler cookieClientHandler = new CookieClientHandler(socket, this.dataFile);
+                System.out.printf("A new client has connected from %s:%d %n", socket.getInetAddress(), socket.getPort());
 
-                System.out.println("Listening on port: " + clientSocket.getLocalPort());
-
-
-                try (DataInputStream inputStream = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()))) {
-
-                    String msg = "";
-                    while (!"close".equals(msg.trim())) {
-                        msg = inputStream.readUTF();
-                        System.out.println("Received: " + msg);
-
-                        if ("get-cookie".equals(msg)) {
-                            System.out.println("Sending cookie to client...");
-                            String msgToClient = "cookie-text " + cookie.get();
-                            System.out.println("└─: " + msgToClient);
-                            out.writeUTF(msgToClient);
-                            out.flush();
-                        }
-                    }
-                    System.out.println("Client closed the connection");
-
-                }
+                Thread thread = new Thread(cookieClientHandler);
+                thread.start();
             }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (this.serverSocket != null) {
+                this.serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
